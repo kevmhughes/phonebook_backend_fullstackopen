cd41efcff037fe4df9b9f721da4a9f68824d3fe1
kevmhughes <kevhughes24@hotmail.com>
2024-12-02 20:37:55 +0100

    3.20*: Phonebook database, step 8

--- models/contact.js
+++ models/contact.js
@@ -1,14 +1,32 @@
 // contact schema
 const { Schema, model } = require("mongoose");
 
+// Capitalize the first letter of each word in a string
+function capitalizeWords(str) {
+  return str.replace(/\b\w/g, char => char.toUpperCase()).replace(/\s+/g, ' ').trim();
+}
+
 const contactSchema = new Schema({
     id: String,
     name: {
       type: String,
       minLength: 3,
-      required: true
+      required: true,
+      match: /^[A-Za-z\s]+$/, // Only letters and spaces are allowed
+      set: capitalizeWords  // Automatically capitalize first letter of each word before saving
     },
-    number: String
+    number: {
+      type: String,
+      minLength: 8,
+      required: true,
+      validate: {
+        validator: function(v) {
+        // The regex matches phone numbers with 2 or 3 digits, a hyphen, and 6 to 9 digits after the hyphen.
+        return /^\d{2,3}-\d{6,9}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+    }
 })
 
 contactSchema.set('toJSON', {
